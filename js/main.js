de391efa79f24e3ffba0f43dd29f25641d3cd143
Dapo Ola-Olatunji <71828782+Nehemiah01@users.noreply.github.com>
2022-05-31 23:32:23 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+let hamburger = document.querySelector('.hamburger-menu')
+let links = document.querySelector('.links')
+let getStarted = document.querySelector('.get-started')
+
+console.log(hamburger)
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('cancel')
+    links.classList.toggle('display')
+    getStarted.classList.toggle('display')
+})
